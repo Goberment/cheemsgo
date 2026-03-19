3e7e001b467f8ce146e33252add060cff3cb2479
Goberment <diegofco@gmail.com>
2026-03-19 12:49:46 -0700

    Preparacion del ambiente de Flask y hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def index():
+    return jsonify({"succes": True, "message": "Helo world"}), 200
+
+
+if __name__ == "__main__":
+    app.run(port=5001)
