8a7c0cf89bd68db34f3f89ce868bf967abe5c821
Goberment <diegofco@gmail.com>
2026-05-18 22:08:21 -0700

    cambio de errores del port

--- persistence/db.py
+++ persistence/db.py
@@ -4,7 +4,7 @@ import os
 def get_connection():
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", "3306")),
+        port=int(os.environ.get("MYSQLPORT") or 3306),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", "nWApZrZCmDTdvHcMYScNqpNRfBRyYNCJ"),
         database=os.getenv("MYSQLDATABASE", "railway")
