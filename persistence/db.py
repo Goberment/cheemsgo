16c9dba2cceeee17b8f03c5e801efbbdd69e7eae
Goberment <diegofco@gmail.com>
2026-05-18 22:02:50 -0700

    Base de datos modificada

--- persistence/db.py
+++ persistence/db.py
@@ -6,7 +6,7 @@ def get_connection():
         host=os.getenv("DB_HOST", "localhost"),
         port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", "goberment31"),
-        database=os.getenv("DB_NAME", "cheemsdb")
+        password=os.getenv("DB_PASSWORD", "nWApZrZCmDTdvHcMYScNqpNRfBRyYNCJ"),
+        database=os.getenv("MYSQLDATABASE", "railway")
     )
     return connection
