8085b9cfc32d3b1d38c50d4f08e41e2b3c252640
Goberment <diegofco@gmail.com>
2026-05-18 22:21:22 -0700

    correccion de errores

--- persistence/db.py
+++ persistence/db.py
@@ -4,9 +4,9 @@ import os
 def get_connection():
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.environ.get("MYSQLPORT") or 3306),
+        port=int(os.environ.get("DB_PORT") or 3306),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", "nWApZrZCmDTdvHcMYScNqpNRfBRyYNCJ"),
-        database=os.getenv("MYSQLDATABASE", "railway")
+        database=os.getenv("DB_NAME", "railway")
     )
     return connection
